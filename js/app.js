4b93831eafb26c875255e7ae10a651a2741ed8d5
gufronnakaaw <gufronnakaaw@gmail.com>
2021-02-02 20:40:42 +0700

    add validation

--- js/app.js
+++ js/app.js
@@ -14,40 +14,54 @@ document.querySelector('.btn-search').addEventListener('click', function(){
     fetch(url).finally(() => {
         document.querySelector('.loading').style.display = "none";
     }).then(response => response.json()).then(result => {
-
-        let movies = result.Search;
-        let cards = '';
-
-        movies.forEach( el => {
-            cards += `<div class="col-md-4 my-4" style="width: 18rem;">
-                        <div class="card">
-                            <img src="${el.Poster}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${el.Title}</h5>
-                                <p class="card-text">${el.Year}</p>
-                                <button class="btn btn-primary btn-show" data-omdbid="${el.imdbID}">Show Details</button>
+        
+        if( result.Response === "True" ) {
+            
+            let movies = result.Search;
+            let cards = '';
+    
+            movies.forEach( el => {
+                cards += `<div class="col-md-4 my-4" style="width: 18rem;">
+                            <div class="card">
+                                <img src="${el.Poster}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">${el.Title}</h5>
+                                    <p class="card-text">${el.Year}</p>
+                                    <button class="btn btn-primary btn-show" data-omdbid="${el.imdbID}">Show Details</button>
+                                </div>
                             </div>
-                        </div>
-                    </div>`;    
-        });
-        
-        document.querySelector('.container .row-movies').innerHTML = cards;
-        
-        document.querySelectorAll('.btn-show').forEach(el => {
-            el.addEventListener('click', function(){
-                const id = this.dataset.omdbid;
-
-                window.location.href = `show.html?id=${id}&search=${inputValue}`;
+                        </div>`;    
             });
-        });
+            
+            document.querySelector('.container .row-movies').innerHTML = cards;
+            
+            document.querySelectorAll('.btn-show').forEach(el => {
+                el.addEventListener('click', function(){
+                    const id = this.dataset.omdbid;
+    
+                    window.location.href = `show.html?id=${id}&search=${inputValue}`;
+                });
+            });
 
-    }).catch(() => {
-        const alert = `<div class="alert alert-danger mx-3 text-center" role="alert" style="width: 100%;">
-                            ${inputValue} not found!
-                    </div>`;
+        } else {
+
+            const alert = `
+            <div class="alert alert-danger mx-3 text-center" role="alert" style="width: 100%;">
+                ${result.Error}
+            </div>`;
+
+            document.querySelector('.container .row-movies').innerHTML = alert;
+
+        }
+
+    }).catch(err => {
+        const alert = `
+        <div class="alert alert-danger mx-3 text-center" role="alert" style="width: 100%;">
+            ${err}, please check your internet connection!
+        </div>`;
 
         document.querySelector('.container .row-movies').innerHTML = alert;
-    })
+    });
 
     document.querySelector('.input-search').value = '';
 });
@@ -68,7 +82,7 @@ if( search !== undefined ){
         let movies = result.Search;
         let cards = '';
 
-        movies.forEach( el => {
+        movies.forEach(el => {
             cards += `<div class="col-md-4 my-4" style="width: 18rem;">
                         <div class="card text-center">
                             <img src="${el.Poster}" class="card-img-top" alt="...">
@@ -97,17 +111,18 @@ if( search !== undefined ){
 }
 
 function getUrlVars(param = null) {
-	if(param !== null) {
-		var vars = [], hash;
-        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	if( param !== null ) {
+		let vars = [], hash;
+        let hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
         
-		for(var i = 0; i < hashes.length; i++) {
+		for( let i = 0; i < hashes.length; i++ ) {
 			hash = hashes[i].split('=');
 			vars.push(hash[0]);
 			vars[hash[0]] = hash[1];
         }
         
-		return vars[param];
+        return vars[param];
+        
     } else {
 		return null;
 	}
